8a931d08d1e06cdbddad95e42cefcc91be695158
David <lmorochorf@gmail.com>
2026-02-11 16:01:01 +0100

    Creé todo los controladores de los modelos, creé 2 request de los modelos(Pago User) y creé 3 rutas de los modelos(estadios, partidos y equipos) puedes agregar mas request y rutas si lo necesitamos

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\EquipoController;
+use App\Http\Controllers\EstadioController;
+use App\Http\Controllers\PartidoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//RUTA ESTADIOS
+Route::resource('estadios',EstadioController::class);
+
+//RUTA PARTIDOS
+Route::resource('partidos',PartidoController::class);
+
+//RUTA EQUIPOS
+Route::resource('equipos',EquipoController::class);
+
